32a4af03b8f839b0bdf48dceae31a4626ffa26f3
Hartman <Rosemary.Hartman@wildlife.ca.gov>
2019-04-25 08:37:01 -0700

    zooplankton blitz 2018

--- blitz2/zoopsblitz2.R
+++ blitz2/zoopsblitz2.R
@@ -20,16 +20,16 @@ View(zoopsfrp)
 
 #add more station information
 stations = read_excel("blitz2/Stations2.xlsx")
-zoopsfrp = merge(zoopsfrp, stations)
+zoopsfrpx = merge(zoopsfrp, stations)
 
 ################################################################################################################################
 #Calculate CPUE
 
 #combine the slides
-totalcells = group_by(zoopsfrp, SampleID) %>% 
+totalcells = group_by(zoopsfrpx, SampleID) %>% 
   summarise(totcells = max(CellNumber), totCountall = sum(Count))
 
-zoo = group_by(zoopsfrp, Station, site, sitetype, sitetype2, Region2, Date, SampleID, Dilution, CommonName,
+zoo = group_by(zoopsfrpx, Station, site, sitetype, sitetype2, Region2, Date, SampleID, Dilution, CommonName,
                 Volume) %>% 
   summarise(totCount = sum(Count))
 
@@ -81,92 +81,87 @@ zooptax2 = group_by(zoop, AnalyLS) %>% summarise(totaltaxa = sum(CPUE, na.rm = T
 
 
 #set up some labels for graphing
-zooLab = c("Amphipoda", "Annelida", "Calanoida", "Cal juv", "Cladocera", "Collembola", 
+zooLab = c("Amphipoda", "Annelida", "Barnacle Nauplii", "Calanoida", "Cal juv", "Cladocera", "Collembola", 
             "Cyclopoda", "Cyclo juv", "Decapoda", "fish","Gastropoda", "Harpacticoida", "Insect adult", "Insect juv",
            "Isopoda", "Mysidea", "Nematoda","Ostracoda", "other", "Rotifera", "Tanaid")
-zoop$AnalyLS = factor(zoop$AnalyLS, labels=zooLab)
+zoop$foo = zoop$AnalyLS
+zoop$AnalyLS = factor(zoop$AnalyLS, labels = zooLab)
 
-
+test = group_by(zoop, AnalyLS, foo) %>% summarise(catch = sum(CPUE))
 #Now let's look at what we got!
 #############################################################################################################
 
 #filter for just samples from the blitz
 zoop$month = month(zoop$Date)
-zooblitz = filter(zoop, month >2 & month <5)
+zoop$year = year(zoop$Date)
+zooblitz = filter(zoop, month >2 & month <5 & year >2016)
+zooblitz = filter(zooblitz, site != "Lindsey" & 
+                    site != "Sherman" & site != "Dow" & site != "Wings")
 
 
 #set up a color pallete
 mypal = c(brewer.pal(9, "Set1"), brewer.pal(12, "Set3"), "black", "white", "grey", "pink")
 
 #put zeros back in to get average catch by species
-zoowide = dcast(zooblitz, SampleID~AnalyLS, fun.aggregate = sum, value.var = CPUE)
+zoowide = dcast(zooblitz, SampleID+Region2+site+Date+month+year+sitetype2~AnalyLS, fun.aggregate = sum, na.rm = T, value.var = "CPUE")
+zooblitz0 = melt(zoowide, id.vars = c("SampleID", "site", "Region2","sitetype2", "year", "Date", "month"), variable.name = "AnalyLS", value.name = "CPUE" )
 
-zooave = group_by(zooblitz, Analy, CommonName, site, AnalyLS) %>% summarize(CPUE = mean(CPUE, na.rm = T))
+zooave = group_by(zooblitz0, site, sitetype2, Region2, AnalyLS, year) %>% summarize(CPUE = mean(CPUE, na.rm = T))
 zooave =droplevels(zooave)
 
 #quick plot of CPUE by location
 z1 = ggplot(zooave, aes(x=site, y= CPUE))
-z1 + geom_bar(stat = "identity", aes(fill = AnalyLS)) + scale_fill_manual(values = mypal)+
-  coord_cartesian(ylim = c(0, 4000))
+z1 + geom_bar(stat = "identity", aes(fill = AnalyLS)) + scale_fill_manual(values = mypal) +
+  coord_cartesian(ylim = c(0, 7000)) + facet_grid(year~Region2, scales = "free")
+
+#relative abundance
+z1 + geom_bar(stat = "identity", aes(fill = AnalyLS), position = "fill") + 
+  scale_fill_manual(values = mypal) +
+  facet_grid(year~Region2, scales = "free_x")
+
 
 #total CPUE
-zootot = group_by(zoo2017, ZSampleID, date, Station, 
-                  Location, Region, SiteType2) %>% summarize(CPUE = sum(CPUE, na.rm = T))
+zootot = group_by(zooblitz0, SampleID, Date, site, year, month, 
+                 Region2, sitetype2) %>% summarize(CPUE = sum(CPUE, na.rm = T))
 
-z2 = ggplot(zootot, aes(x=Location, y= CPUE))
+z2 = ggplot(zootot, aes(x=site, y= CPUE))
 z2+geom_boxplot()
-#One major outlier at Prospect. Let's figure out what it is
+#Bradmoor had crzy high CPUE.
+z2+geom_boxplot() + coord_cartesian(ylim = c(0,5000))
 
-pros = filter(zoo2017, Location =="Prospect" & CPUE >1000)
-#The major outlier is ZOOP3-25APR2017
-#I'm going to take it out to make the analysis easier. 
-zoo2017 = filter(zoo2017, ZSampleID != "ZOOP3-25APR2017")
 
-#recalculate:
-#quick plot of CPUE by location
-zoo2017ave = group_by(zoo2017, Analy, CommonName, Location, AnalyLS, label) %>% summarize(CPUE = mean(CPUE, na.rm = T))
-zoo2017ave =droplevels(zoo2017ave)
-z1 = ggplot(zoo2017ave, aes(x=Location, y= CPUE))
-z1 + geom_bar(stat = "identity", aes(fill = Analy)) + scale_fill_manual(values = mypal)
+#average total cpue for bar plot
 
-#total CPUE
-zootot = group_by(zoo2017, ZSampleID, date, Station, 
-                  Location, Region, SiteType2, label) %>% summarize(CPUE = sum(CPUE, na.rm = T))
-
-z2 = ggplot(zootot, aes(x=Location, y= CPUE))
-z2+geom_boxplot()
-#better
-
-zoototave = group_by(zootot,
-                  Location, Region, SiteType2, label) %>% 
+zoototave = group_by(zootot, site, Region2, sitetype2, year) %>% 
   summarize(mCPUE = mean(CPUE, na.rm = T), seCPUE = sd(CPUE)/length(CPUE), N = length(CPUE))
 
-z3 = ggplot(zoototave, aes(x=label, y=mCPUE))
-z3 + geom_bar(stat = "identity", aes(fill = Location)) +
-  facet_grid(.~Region, scales = "free", space = "free_x") + 
+z3 = ggplot(zoototave, aes(x=site, y=mCPUE))
+z3 + geom_bar(stat = "identity", aes(fill = site)) +
+  facet_grid(year~Region2, scales = "free", space = "free_x") + 
   geom_errorbar(aes(ymin = mCPUE - seCPUE, ymax = mCPUE + seCPUE)) +
   scale_fill_manual(values = mypal, guide = "none")+
   geom_label(aes(label = paste("n = ", N), y = mCPUE+ 300), label.padding = unit(0.1, "lines"), size = 4) +
-  mytheme + ylab("CPUE") + xlab(label = NULL)
+   ylab("CPUE") + xlab(label = NULL)+
+coord_cartesian(ylim = c(0,10000))
 
 #GLMm of total CPUE
-zootot$SiteType2 = as.factor(zootot$SiteType2)
-zblitz = lmer(log(CPUE) ~ Region + SiteType2 + (1|Location), data = zootot)
+zootot$sitetype2 = as.factor(zootot$sitetype2)
+zblitz = lmer(log(CPUE) ~ Region2 + sitetype2 + (1|site), data = zootot)
 summary(zblitz)
 visreg(zblitz)
-#Differences by region, but not by habitat type. Opposite of macroinvertebrates!!!!
-zblitza = glm(log(CPUE) ~ Region + SiteType2, data = zootot)
+#Differences by site type, not region
+zblitza = glm(log(CPUE) ~ Region2 + sitetype2, data = zootot)
 summary(zblitza)
 visreg(zblitza)
 
-zblitzb = aov(log(CPUE) ~ Region + SiteType2 + Location, data = zootot)
+zblitzb = aov(log(CPUE) ~ Region2 + sitetype2 + site,  data = zootot)
 summary(zblitzb)
 TukeyHSD(zblitzb)
 #################################################################################
 
 #calculate coefficient of variation in CPUE
-zCVs = summarize(group_by(zootot, Location), mCPUE = mean(CPUE), 
-                sdCPUE = sd(CPUE), seCPUE = sd(CPUE)/length(CPUE), N = length(ZSampleID), CV = sdCPUE/mCPUE)
+zCVs = summarize(group_by(zootot, site), mCPUE = mean(CPUE), 
+                sdCPUE = sd(CPUE), seCPUE = sd(CPUE)/length(CPUE), N = length(SampleID), CV = sdCPUE/mCPUE)
 
 #Calculate average withing-site CV and the CV of within-site means by sampling type.
 zmCVs = summarize(zCVs, mCV = mean(CV, na.rm = T), mmCPUE = mean(mCPUE), 
@@ -176,73 +171,82 @@ zmCVs = summarize(zCVs, mCV = mean(CV, na.rm = T), mmCPUE = mean(mCPUE),
 ############################################################################################
 
 #plot of community composition 
-z4 = ggplot(zoo2017, aes(x=label, y=CPUE))
+z4 = ggplot(zooblitz, aes(x=site, y=CPUE))
 z4 + geom_bar(stat = "identity", aes(fill = AnalyLS), position = "fill") +
-  facet_grid(.~Region, scales = "free", space = "free_x") + 
-  scale_fill_manual(values = c(mypal, "grey"), labels = zooLab, name = NULL) +
-  mytheme + ylab("Relative Abudance") +xlab(label = NULL)
+  facet_grid(year~Region2, scales = "free", space = "free_x") + 
+  scale_fill_manual(values = c(mypal, "grey"), name = NULL) +
+   labs(y="Relative Abudance", x=NULL)
 
 #plot of community composition by region
-z4 = ggplot(zoo2017, aes(x=Region, y=CPUE))
+z4 = ggplot(zooblitz, aes(x=Region2, y=CPUE))
 z4 + geom_bar(stat = "identity", aes(fill = AnalyLS), position = "fill") +
   
   scale_fill_manual(values = c(mypal, "grey"), labels = zooLab, name = NULL) +
-  mytheme + ylab("Relative Abudance") +xlab(label = NULL)
+  ylab("Relative Abudance") +xlab(label = NULL)
 
 
 #plot of community composition by site type
-z4 = ggplot(zoo2017, aes(x=SiteType2, y=CPUE))
+z4 = ggplot(zooblitz, aes(x=sitetype2, y=CPUE))
 z4 + geom_bar(stat = "identity", aes(fill = AnalyLS), position = "fill") +
   
   scale_fill_manual(values = c(mypal, "grey"), labels = zooLab, name = NULL) +
-  mytheme + ylab("Relative Abudance") +xlab(label = NULL)
+   ylab("Relative Abudance") +xlab(label = NULL)
 
 #Community matrix by CPUE
-zMat = dcast(zoo2017, formula = ZSampleID~AnalyLS, value.var="CPUE", 
-             fun.aggregate = sum, fill = 0)
-zMat = zMat[,2:19]
+zMat = zoowide[,8:28]
 #now by proprotions
 zMatp = zMat/rowSums(zMat)
 
 #PerMANOVA
-za1 = adonis(zMatp~ Region + SiteType2 + Location, data = zootot)
+za1 = adonis(zMatp~ Region2 + sitetype2 + year, data = zootot)
 za1
-#region and site type are both significant, but site type much more so than region.
+#region and site type are both significant, and year
 
-zNMDS = metaMDS(zMatp, try = 50)
+zNMDS = metaMDS(zMatp, try = 1000)
+zNMDS
+#no convergent solutions
+
+#but let's graph it anyway!
+zootot$Region2 = as.factor(zootot$Region2)
+source("PlotNMDS.R")
+PlotNMDS(zNMDS, data = zootot, group = "Region2")
+
+zootot$sitetype2 = as.factor(zootot$sitetype2)
+PlotNMDS2(zNMDS, data = zootot, group = "sitetype2")
+
+#Hm. Last year, Tule red through everyting off, but now it's all a mess anyway, so that's less of an issue.
+#I'm just going to run the 2018 data
+
+#######################################################################################################
+
+
+#Community matrix by CPUE
+zMat2018 = filter(zoowide, year != 2017)[,8:27]
+#now by proprotions
+zMat2018p = zMat2018/rowSums(zMat2018)
+
+zootot2018 = filter(zootot, year != 2017)
+
+#PerMANOVA
+za118 = adonis(zMat2018p~ Region2 + sitetype2, data = zootot2018)
+za118
+#region and site type are both significant, 
+
+zNMDS = metaMDS(zMat2018p, try = 1000)
 zNMDS
 
-zootot$Region = as.factor(zootot$Region)
-PlotNMDS2(zNMDS, data = zootot, group = "Region", xlimit = c(-.8, 0.8), ylimit = c(-.8, .8))
+zootot2018$Region2 = droplevels(as.factor(zootot2018$Region2))
+zootot2018$sitetype2 = droplevels(as.factor(zootot2018$sitetype2))
+source("PlotNMDS.R")
+PlotNMDS(zNMDS, data = zootot2018, group = "sitetype2")
+PlotNMDS(zNMDS, data = zootot2018, group = "Region2")
 
-zootot$SiteType2 = as.factor(zootot$SiteType2)
-PlotNMDS2(zNMDS, data = zootot, group = "SiteType2", xlimit = c(-.8, 3), ylimit = c(-.8, .8))
 
-#So, Tule red was really wierd. LEt's try re-running it again without tule red
-
-zMatp1 = zMatp[which(zootot$Location != "Tule Red"),]
-
-#run the Permanova again
-za2 = adonis(zMatp1~ SiteType2 + Region, data = filter(zootot, Location != "Tule Red"))
-za2
-
-#Run the NMDS again
-zNMDS2 = metaMDS(zMatp1, try = 50)
-zNMDS2
-
-#plot it
-zootot$Region = as.factor(zootot$Region)
-zootot$SiteType2 = as.factor(zootot$SiteType2)
-PlotNMDS(zNMDS2, data = filter(zootot, Location != "Tule Red"), group = "Region")
-envfit(zNMDS2~Region, data = filter(zootot, Location != "Tule Red"))
-
-PlotNMDS(zNMDS2, data = filter(zootot, Location != "Tule Red"), group = "SiteType2")
-envfit(zNMDS2~SiteType2, data = filter(zootot, Location != "Tule Red"))
 
 ########################################################################################################
 #Look at catch at Tule Red to figure out what in the "other" category was so dominant
 
-TR = droplevels(filter(zoop, Location == "Tule Red"))
+TR = droplevels(filter(zoop, site == "Tule Red"))
 TR1 = ggplot(TR, aes(x=CommonName, y = CPUE))
 TR1 + geom_bar(stat = "identity") + theme(axis.text.x = element_text(angle = 90))
 
@@ -252,3 +256,12 @@ TR2 + geom_bar(stat = "identity") + theme(axis.text.x = element_text(angle = 90)
 others = filter(TR, Analy == "other")
 TR3 = ggplot(others, aes(x = CommonName, y = CPUE))
 TR3 + geom_bar(stat = "identity")
+
+#check Grizzly bay too
+griz = droplevels(filter(zoop, site == "Grizzly"))
+gr = ggplot(griz, aes(x=CommonName, y = CPUE))
+gr + geom_bar(stat = "identity") + theme(axis.text.x = element_text(angle = 90))
+
+others = filter(griz, Analy == "other")
+gr3 = ggplot(others, aes(x = CommonName, y = CPUE))
+gr3 + geom_bar(stat = "identity")
